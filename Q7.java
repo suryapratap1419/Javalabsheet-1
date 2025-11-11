0af3a8595d9dea910dc1be63fc160acc92ecb237
Surya Pratap <suryapratap2154@gmail.com>
2025-11-11 16:18:07 +0530

    Add DivisibilityCheck program in Q7.java

---
+++ Q7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DivisibilityCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println(number + " is divisible by both 3 and 5.");
+        } else {
+            System.out.println(number + " is NOT divisible by both 3 and 5.");
+        }
+        
+        scanner.close();
+    }
+}

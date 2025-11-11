b066ad5bfaae523ded67d276a1df01d6d4108f8e
Surya Pratap <suryapratap2154@gmail.com>
2025-11-11 16:11:24 +0530

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String name = "Surya Pratap";
+        int age = 22;
+        String collegeName = "College of Engineering Roorkee";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("College: " + collegeName);
+    }
+}
